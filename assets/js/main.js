83379cd57401a09261e6389424a626f537ced371
norman <nbukingolts@ufl.edu>
2025-02-16 20:36:17 -0500

    Adds horizontal padding to carousel, box shadow styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,6 +85,7 @@ function carousel() {
     carousel.owlCarousel({
         dots: false,
         margin: 32,
+        stagePadding: 24,
         nav: true,
         navText: [
             '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22" height="22" fill="currentColor"><path d="M20.547 22.107l-6.107-6.107 6.107-6.12-1.88-1.88-8 8 8 8 1.88-1.893z"></path></svg>',
